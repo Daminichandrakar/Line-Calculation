671288e314f4e1066288ee9e962d9772d1b7ecd1
Daminichandrakar <damini.2201@gmail.com>
2021-08-27 23:00:22 +0530

    Added: apply oops concept

--- LineCalculation.java
+++ LineCalculation.java
@@ -5,33 +5,53 @@ import java.util.Scanner;
 public class LineCalculation {
 
 	public static void main(String[] args) {
+
+		LineCalculation lineCalculation = new LineCalculation();
+		lineCalculation.input();
+	}
+
+	public void input() {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter x1 = " );
+		System.out.println("Enter x1 = ");
 		double x1 = scan.nextDouble();
-		System.out.println("Enter y1 = " );
+		System.out.println("Enter y1 = ");
 		double y1 = scan.nextDouble();
-		System.out.println("Enter x2 = " );
+		System.out.println("Enter x2 = ");
 		double x2 = scan.nextDouble();
-		System.out.println("Enter y2 = " );
+		System.out.println("Enter y2 = ");
 		double y2 = scan.nextDouble();
-		System.out.println("Enter x3 = " );
+		System.out.println("Enter x3 = ");
 		double x3 = scan.nextDouble();
-		System.out.println("Enter y3 = " );
+		System.out.println("Enter y3 = ");
 		double y3 = scan.nextDouble();
-		System.out.println("Enter x4 = " );
+		System.out.println("Enter x4 = ");
 		double x4 = scan.nextDouble();
-		System.out.println("Enter y4 = " );
+		System.out.println("Enter y4 = ");
 		double y4 = scan.nextDouble();
-		//calculating distane
-		double dist = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-		double dist1 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
+		double dist = Calculation.calculate(x1, y1, x2, y2);
+		double dist1 = Calculation.calculate(x3, y3, x4, y4);
+		;
 		String line1 = Double.toString(dist);
 		String line2 = Double.toString(dist1);
 		Boolean equal = line1.equals(line2);
+		Compare.compare(line1, line1);
+	}
+
+}
+
+class Calculation {
+
+	public static double calculate(double x1, double y1, double x2, double y2) {
+		double dist = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		return dist;
+	}
+}
+
+class Compare {
+	public static void compare(String line1, String line2) {
 		int compare = line1.compareTo(line2);
-		
 		if (compare == 0) {
-			System.out.println("Equal");
+			System.out.println("Line1 or Line2 is Equal");
 		} else if (compare >= 1) {
 			System.out.println("Line1 Greater then Line2 ");
 		} else {
