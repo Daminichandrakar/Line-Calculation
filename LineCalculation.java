30124bd46aecafd17e4ce3293061eb6cbfc1f4dd
Daminichandrakar <damini.2201@gmail.com>
2021-08-27 22:23:15 +0530

    Added: calculating two line distance and check equal or not

--- LineCalculation.java
+++ LineCalculation.java
@@ -3,8 +3,16 @@ package com.bl.java;
 public class LineCalculation {
 
 	public static void main(String[] args) {
-		double x1 = 5, y1 = 5, x2 = 5, y2 = 0;
-		double dist = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-		System.out.println(dist);
+		double x1 = 5, y1 = 5, x2 = 5, y2 = 0, x3 = 5, y3 = 9, x4 = 8, y4 = 0;
+		double dist = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));//line 1 distance
+		double dist1 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));//line 2 distance
+		String line1 = Double.toString(dist);
+		String line2 = Double.toString(dist1);
+		Boolean equal = line1.equals(line2);
+		if (equal == true) {
+			System.out.println("Equal");
+		} else {
+			System.out.println("Not Equal");
+		}
 	}
 }
