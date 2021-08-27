61089f7135564d1df2d49e7cafc3c859e58d707e
Daminichandrakar <damini.2201@gmail.com>
2021-08-27 22:19:24 +0530

    Added: calculating line distance

--- LineCalculation.java
+++ LineCalculation.java
@@ -3,6 +3,8 @@ package com.bl.java;
 public class LineCalculation {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to line calculation");
+		double x1 = 5, y1 = 5, x2 = 5, y2 = 0;
+		double dist = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		System.out.println(dist);
 	}
 }
